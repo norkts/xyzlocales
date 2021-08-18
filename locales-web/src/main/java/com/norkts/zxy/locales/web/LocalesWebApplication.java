030b5874217ea1aedb210be05744320513757491
norkts <huangce.zb@alibaba-inc.com>
2021-08-18 08:33:32 +0800

    finish util

---
+++ locales-web/src/main/java/com/norkts/zxy/locales/web/LocalesWebApplication.java
@@ -0,0 +1,13 @@
+package com.norkts.zxy.locales.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalesWebApplication.class, args);
+    }
+
+}
---
+++ locales-web/src/test/java/com/norkts/zxy/locales/web/LocalesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norkts.zxy.locales.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
